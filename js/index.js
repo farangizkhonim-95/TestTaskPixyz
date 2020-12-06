09181cc99052dca8ae7ef7e00a9535ee6129034d
unknown <khonim1995@gmail.com>
2020-12-06 11:37:01 +0500

    My first Commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 5
+        },
+        1000: {
+            items: 8
+        }
+
+    }
+
+
+    
+    
+    })
+
+    
